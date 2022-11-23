1a0b3b74e59ac797c14204998173b60a3cc530e9
j-jagan <118331861+j-jagan@users.noreply.github.com>
2022-11-23 16:04:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ terraform {
 provider "azurerm" {
   features {}
   subscription_id = "6376be33-6dc5-4eb5-8faf-84204ce9e3ed"
-  client_id       = "013e9a6f-b06b-430f-9d9a-17782a050f95" 
-  client_secret   = "QJj8Q~-Mmx_asxkq15gz7dddV2lQHCmTXakTxdoy"
+  client_id       = "d98eb71d-64ca-484b-8f74-abc6931514fd" 
+  client_secret   = "xZP8Q~PkCgo8lLw_g2qqjVLzQubcSUGfW~rLqbEz"
   tenant_id       = "ad812bb6-e9a8-4136-9d99-0d17ea56fbc9"
 
 }
